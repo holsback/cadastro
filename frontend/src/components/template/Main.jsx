d35eb972698c1dbb3fa6a13dc3bfdec00ee6bd8a
MarcoTulio25 <marcotulio25senai@gmail.com>
2019-08-20 14:44:21 -0300

    Footer e Main

---
+++ frontend/src/components/template/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Main.css'
+import Header from './Header'
+//Main será nosso executável
+export default props =>/*Componente pode retornar multiplos elementos.
+Os fragmentos permitem agrupar uma lista de filhos sem adicionar nos extras ao DOM
+Para usarmos o Header*/
+    <React.Fragment>
+        <Header />
+        <main className = "content">
+            Conteúdo
+        </main>
+    </React.Fragment>
