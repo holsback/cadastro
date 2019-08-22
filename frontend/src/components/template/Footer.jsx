0ff5af0e17e745670ca57a8477aa47773a1e89fa
MarcoTulio25 <marcotulio25senai@gmail.com>
2019-08-22 15:18:17 -0300

    Footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import './Footer.css'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className = "fa fa-heart text-danger">
+            </i> por <strong> Bruno Holsback</strong>
+        </span>
     </footer>
