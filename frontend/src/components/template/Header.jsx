dcf01954dcdebbd6c4e15dc1df0a37a464065e1b
MarcoTulio25 <marcotulio25senai@gmail.com>
2019-08-22 14:17:09 -0300

    Header

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Header.css'
 
 export default props =>
-    <header className="reader">
-        Cabeçalho
+    <header className="header d-none d-sm-flex flex-column">
+        <h1 className = "mt-3">
+            <i className={`fa fa-${props.icon}`}></i> {props.title}
+        </h1>
     </header>
