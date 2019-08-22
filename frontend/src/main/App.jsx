8b142b555b6a13c1128bad9859e101aabf16fda1
MarcoTulio25 <marcotulio25senai@gmail.com>
2019-08-22 13:53:48 -0300

    App

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
