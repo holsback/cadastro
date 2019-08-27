49f216f4645aed93db3a6284ab0d963fcd2ed438
holsback <holsback26@gmail.com>
2019-08-27 14:36:48 -0300

    UserCrud

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
 import Main from '../template/Main'
+import { returnStatement } from '@babel/types';
 
 const heardProps = {
     icon: 'users',
     title: 'Usuários',
+    subtitle: 'Cadastro: Incluir, Lista, Alterar e Excluir!'
+}
+
+export default class UserCrud extends Component {
+    render(){
+        retur(
+            <Main {...heardProps}>
+                Teste de cadastro
+            </Main>
+        )
+    }
 }
