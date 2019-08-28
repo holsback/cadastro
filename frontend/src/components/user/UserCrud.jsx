21c57db7b280685cd1c249e0ffede6e986f6311d
Ryanprado99 <alancsystem@gmail.com>
2019-08-28 14:48:16 -0300

    json

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import Main from '../template/Main'
-import { returnStatement } from '@babel/types';
+import axios from 'axios'
 
 const heardProps = {
-    icon: 'users',
+    icon: 'users text-danger',
     title: 'Usuários',
     subtitle: 'Cadastro: Incluir, Lista, Alterar e Excluir!'
+}
+
+//Localização do banco
+const baseUrl = "http://localhost:3001/user"
+//Estado inicial - Quando sobe a aplicação
+const inicialState = {
+    user: {name: '', email: ''},
+    list: []
 }
 
 export default class UserCrud extends Component {
