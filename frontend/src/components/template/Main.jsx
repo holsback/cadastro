543243563e035647829b437c8f3f29f077d7a948
Ryanprado99 <alancsystem@gmail.com>
2019-08-29 15:09:57 -0300

    Main

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './Main.css'
 import Header from './Header'
 //Main será nosso executável
-export default props =>/*Componente pode retornar multiplos elementos.
-Os fragmentos permitem agrupar uma lista de filhos sem adicionar nos extras ao DOM
-Para usarmos o Header*/
+export default props =>/*componente pode retornar múltiplos 
+elementos. Os Fragmentos permitem agrupar uma lista 
+de filhos sem adicionar nós extras ao DOM. PARA USARMOS O HEADER*/
     <React.Fragment>
         <Header {...props}/>
         <main className = "content container-fluid">
