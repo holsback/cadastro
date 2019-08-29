4797b82d9c7341c2076f983d87dc45700870a24a
Ryanprado99 <alancsystem@gmail.com>
2019-08-29 15:12:31 -0300

    Home

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Main from '../template/Main'
 
 export default props =>
-    <Main icon="graduation-cap text-danger" title="Sesi Senai"
-            subtitle="Ciência, robótica, inovação e tecnologia">
-            <div className = 'display-4'>Teste de conteúdo</div>
-                <hr />
-                <p className="mb-0">Cadastro</p>
-    </Main>
+        <Main icon="graduation-cap text-danger" title="Sesi Senai"
+            subtitle="Ciência, robótica, inovação e tecnologia para o dia a dia dos nossos alunos">
+            <div className = 'displey-4'>Teste de conteúdo</div>
+            <hr />
+            <p className="mb-0">Cadastro</p>
+        </Main>
+        
