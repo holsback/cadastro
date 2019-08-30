1490017719bf5240ab6ad834f7626afe8c9244eb
Ryanprado99 <alancsystem@gmail.com>
2019-08-30 15:17:03 -0300

    bd.Json

--- frontend/src/components/Produto/ProdutoCrud.jsx
+++ frontend/src/components/Produto/ProdutoCrud.jsx
@@ -3,20 +3,20 @@ import Main from "../template/Main";
 import axios from "axios";
 
 const heardProps = {
-  icon: "fab fa-product-hunt text-danger",
+  icon: "produto text-danger",
   title: "Produtos",
-  subtitle: "Produtos: Incluir, Lista, Alterar e Excluir!"
+  subtitle: "Cadastro: Incluir, Lista, Alterar e Excluir!"
 };
 
 //Localizando nosso banco
 const baseUrl = "http://localhost:3001/produtos";
 //Estado inicial - Quando sobe a aplicação
 const inicialState = {
-  produtos: { NomeProduto: "", Quantidade: "", Valor: "" },
+  produto: { Produto: "", quantidade: "" ,valor: "" ,},
   list: []
 };
 
-export default class ProdutoCrud extends Component {
+export default class ProdCrud extends Component {
   state = { ...inicialState };
 
   //Será chamado antes do componente na tela
@@ -28,41 +28,41 @@ export default class ProdutoCrud extends Component {
   }
 
   clear() {
-    this.setState({ produtos: inicialState.produtos });
+    this.setState({ produto: inicialState.produto });
   }
   //Para incluir e alterar
   save() {
-    const produtos = this.state.produtos;
-    const metodo = produtos.id ? "put" : "post";
+    const produto = this.state.produto;
+    const method = produto.id ? "put" : "post";
     /*Se id for verdadeiro (existe um id, faça um put),
         senao um post */
-    const url = produtos.id ? `${baseUrl}/${produtos.id}` : baseUrl;
+    const url = produto.id ? `${baseUrl}/${produto.id}` : baseUrl;
     //Se existe um id atualiza a informação senão baseUrl cria mais um id
-    axios[metodo](url, produtos).then(resp => {
+    axios[method](url, produto).then(resp => {
       //getUpdateLIst será criada
       const list = this.getUpdateList(resp.data);
-      this.setState({ produtos: inicialState.produtos, list });
+      this.setState({ produto: inicialState.produto, list });
     });
   }
   //Atualizando a lista
-  getUpdateList(produtos) {
+  getUpdateList(produto) {
     //Cria uma nova lista a partir do filter
     //u => cria uma lista a separando o id que passou
     //Unshift coloca esse id na primeira posição do array
     //return list atualiza a linha 35 que atualiza o estado.
-    const list = this.state.list.filter(u => u.id !== produtos.id);
-    list.unshift(produtos);
+    const list = this.state.list.filter(p => p.id !== produto.id);
+    list.unshift(produto);
     return list;
   }
 
   updateField(event) {
     //user será o clone (ou recebe o valor) do estado user
     //clonamos para não alterar o objeto direatamente
-    const produtos = { ...this.state.produtos };
+    const produto= { ...this.state.produto };
     //seta o que está em input e virá value
-    produtos[event.target.NomeProduto] = event.target.value;
+    produto[event.target.name] = event.target.value;
     //set insere
-    this.setState({ produtos });
+    this.setState({ produto });
   }
   //Jsx para renderizar o formulário.
   renderForm() {
@@ -70,14 +70,14 @@ export default class ProdutoCrud extends Component {
       <div className="form">
         <div className="col-12 col-md-6">
           <div className="form-group">
-            <label>Nome Produto</label>
+            <label>Produto</label>
             <input
               type="text"
               className="form-control"
-              name="produtos"
-              value={this.state.produtos.NomeProduto}
+              name="Produto"
+              value={this.state.produto.Produto}
               onChange={e => this.updateField(e)}
-              placeholder="Digite o nome do produto"
+              placeholder="Nome do produto"
             />
           </div>
         </div>
@@ -87,28 +87,26 @@ export default class ProdutoCrud extends Component {
             <input
               type="text"
               className="form-control"
-              name="produtos"
-              value={this.state.produtos.Quantidade}
+              name="quantidade"
+              value={this.state.produto.quantidade}
               onChange={e => this.updateField(e)}
               placeholder="Digite a quantidade"
             />
           </div>
         </div>
-
         <div className="col-12 col-md-6">
           <div className="form-group">
             <label>Valor</label>
             <input
-              type="text"
-              className="form-control"
-              name="produtos"
-              value={this.state.produtos.Valor}
-              onChange={e => this.updateField(e)}
-              placeholder="Digite o valor"
+            type="text"
+            className="form-control"
+            name="valor"
+            value={this.state.produto.valor}
+            onChange={e => this.updateField(e)}
+            placeholder="Digite o Valor"
             />
-          </div>
+            </div>
         </div>
-
         <hr />
         <div className="row">
           <div className="col-12 d-flex justify-content-end">
@@ -128,14 +126,14 @@ export default class ProdutoCrud extends Component {
   }
 
   //Atualizar o estado do objeto
-  load(produtos) {
-    this.setState({ produtos });
+  load(produto) {
+    this.setState({ produto });
   }
 
-  remove(produtos) {
+  remove(produto) {
     //Deleta na base então repasa a lista atualizando
-    axios.delete(`${baseUrl}/${produtos.id}`).then(resp => {
-      const list = this.state.list.filter(u => u !== produtos);
+    axios.delete(`${baseUrl}/${produto.id}`).then(resp => {
+      const list = this.state.list.filter(p => p !== produto);
       this.setState({ list });
     });
   }
@@ -145,9 +143,9 @@ export default class ProdutoCrud extends Component {
       <table className="table mt-4">
         <thead>
           <th>ID</th>
-          <th>Nome do Produto</th>
-          <th>Quantidade</th>
-          <th>Valor</th>
+          <th>Produto</th>
+          <th>quantidade</th>
+          <th>valor</th>
           <th>Ações</th>
         </thead>
         <tbody>{this.renderRows()}</tbody>
@@ -155,19 +153,19 @@ export default class ProdutoCrud extends Component {
     );
   }
   renderRows() {
-    return this.state.list.map(produtos => {
+    return this.state.list.map(produto => {
       return (
-        <tr key={produtos.id}>
-          <td>{produtos.id}</td>
-          <td>{produtos.NomeProduto}</td>
-          <td>{produtos.Quantidade}</td>
-          <td>{produtos.Valor}</td>
+        <tr key={produto.id}>
+          <td>{produto.id}</td>
+          <td>{produto.Produto}</td>
+          <td>{produto.quantidade}</td>
+          <td>{produto.valor}</td>
           <td>
             <button className="btn btn=warning">
-              <i className="fa fa-pencil" onClick={() => this.load(produtos)} />
+              <i className="fa fa-pencil" onClick={() => this.load(produto)} />
             </button>
             <button className="btn btn-danger ml-2">
-              <i className="fa fa-trash" onClick={() => this.remove(produtos)} />
+              <i className="fa fa-trash" onClick={() => this.remove(produto)} />
             </button>
           </td>
         </tr>
